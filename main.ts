925491e1f65d4f3fc7dd7e892a9bc96aff9a0dd6
zpezlwf <81898741+zpezlwf@users.noreply.github.com>
2021-04-04 15:09:05 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 basic.forever(function () {
-    for (let a = 0; a <= 4; a++) {
-        led.plot(a, 0)
-        basic.pause(1000)
+    for (let b = 0; b <= 4; b++) {
+        for (let a = 0; a <= 4; a++) {
+            led.plot(a, b)
+            basic.pause(500)
+        }
     }
     basic.clearScreen()
     basic.pause(1000)
